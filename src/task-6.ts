f8c6bf75653fb4f3652cb13b11ecb2d0494d02b0
haaniki <annaanikeeva2020@gmail.com>
2026-06-20 15:31:13 +0200

    fix-honework

--- src/task-6.ts
+++ src/task-6.ts
@@ -2,6 +2,6 @@ function getFirstElement<T>(arr: T[]): T{
   return arr[0];
 }
 
-console.log(getFirstElement([1, 2, 3]), getFirstElement<number>([1, 2, 3]));           // 1
-console.log(getFirstElement(["a", "b", "c"]), getFirstElement<string>(["a", "b", "c"]));     // "a"
-console.log(getFirstElement([true, false, true]), getFirstElement<boolean>([true, false, true])); // true
+console.log("getFirstElement([1, 2, 3])", getFirstElement<number>([1, 2, 3]));           // 1
+console.log('getFirstElement(["a", "b", "c"])', getFirstElement<string>(["a", "b", "c"]));     // "a"
+console.log("getFirstElement([true, false, true])", getFirstElement<boolean>([true, false, true])); // true
